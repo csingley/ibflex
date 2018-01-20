98bc3fee231422da0df5a69316c4b295649738e2
Christopher Singley <csingley@gmail.com>
2018-01-20 01:39:25 -0700

    parser.py - heavy refactor

--- ibflex/parser.py
+++ ibflex/parser.py
@@ -1,5 +1,8 @@
 # coding: utf-8
 """
+Parser/type converter for data in Interactive Brokers' Flex XML format.
+
+https://www.interactivebrokers.com/en/software/reportguide/reportguide.htm#reportguide/activity_flex_query_reference.htm
 """
 # stdlib imports
 import xml.etree.ElementTree as ET
@@ -10,115 +13,129 @@ from ibflex import schemata
 from ibflex.schemata import elementSchemata
 
 
-class FlexQueryResponse(object):
-    """ Simple container for parsed <FlexQueryResponse> data """
-    def __init__(self, queryName, type):
-        self.queryName = queryName
-        self.type = type
-
-    def __repr__(self):
-        return ('FlexQueryResponse(queryName={}, type={}, '
-                'len(flexStatements)={})').format(repr(self.queryName),
-                                                  repr(self.type),
-                                                  len(self.flexStatements))
-
-
-class FlexStatement(object):
-    """ Simple container for parsed <FlexStatement> data """
-    def __init__(self, accountId, fromDate, toDate, period, whenGenerated):
-        self.accountId = accountId
-        self.fromDate = fromDate
-        self.toDate = toDate
-        self.period = period
-        self.whenGenerated = whenGenerated
-
-    def __repr__(self):
-        rep = ('FlexStatement(accountId={}, fromDate={}, toDate={}, '
-               'period={}, whenGenerated={}')
-        rep_vals = [repr(self.accountId), repr(self.fromDate),
-                    repr(self.toDate), repr(self.period),
-                    repr(self.whenGenerated)]
-        
-        init_args = ['accountId', 'fromDate', 'toDate', 'period',
-                     'whenGenerated']
-        lists = [(attr, getattr(self, attr)) for attr in dir(self)
-                 if attr not in init_args
-                 and not attr.startswith('_')
-                 and len(getattr(self, attr)) > 0]
-        for attr, list_ in lists:
-            rep += ', len(' + attr + ')={}'
-            rep_vals.append(len(list_))
-
-        rep += ')'
-        return rep.format(*rep_vals)
+class FlexParserError(Exception):
+    """ Base class for errors in this module """
+    pass
 
 
 class FlexResponseParser(object):
     def __init__(self, source):
         self.source = source
-        self.statements = []
 
     def parse(self):
         tree = ET.ElementTree()
         tree.parse(self.source)
         root = tree.getroot()
-        response = schemata.FlexQueryResponse.convert(root)
+        return self.parse_response(root)
 
-        flexStmts = root.findall('FlexStatements')
+    @classmethod
+    def parse_response(cls, elem):
+        response = schemata.FlexQueryResponse.convert(elem)
+        flexStmts = elem.findall('FlexStatements')
+
+        # Sanity check
         if len(flexStmts) != 1:
             msg = ("FlexQueryResponse must contain exactly 1 FlexStatements, "
                    "not {}")
-            raise ValueError(msg.format(len(flexStmts)))
-        flexStmts = flexStmts[0]
-        flexStmts_attrs = schemata.FlexStatements.convert(flexStmts)
-        count = flexStmts_attrs['count']
-        flexStmt = flexStmts.findall('FlexStatement')
+            raise FlexParserError(msg.format(len(flexStmts)))
+        response['FlexStatements'] = cls.parse_stmts(flexStmts[0])
+
+        return response
+
+    @classmethod
+    def parse_stmts(cls, elem):
+        attrs = schemata.FlexStatements.convert(elem)
+        count = attrs['count']
+        flexStmt = elem.findall('FlexStatement')
+
+        # Sanity check
         if len(flexStmt) != count:
             msg = ("FlexStatements declares count={} "
                    "but contains {} FlexStatement elements")
-            raise ValueError(msg.format(count, len(flexStmt)))
+            raise FlexParserError(msg.format(count, len(flexStmt)))
 
-        response['FlexStatements'] = [self.parse_stmt(stmt) for stmt in flexStmt]
-        return response
+        return [cls.parse_stmt(stmt) for stmt in flexStmt]
 
-    def parse_stmt(self, elem):
+    @classmethod
+    def parse_stmt(cls, elem):
         stmt = schemata.FlexStatement.convert(elem)
-        for child in elem:
-            if child.tag == 'AccountInformation':
-                acctinfo = schemata.AccountInformation.convert(elem)
-                stmt['AccountInformation'] = acctinfo
-            elif child.tag == 'ConversionRates':
-                # IB ConversionRates are always forex to account base currency
-                # Store as dict keyed by (currency, date)
-                schema = schemata.ConversionRate()
-                baseCurrency = stmt['AccountInformation']['currency']
-
-                stmt['ConversionRates'] = {self.parse_rate(schema, elem, baseCurrency) for elem in child}
-            elif child.tag == 'FxPositions':
-                # FxPositions contains FxLots list; maybe more?
-                for grandchild in child:
-                    attrName, items = self.parse_list(grandchild)
-                    stmt[attrName] = items
-            else:
-                # All other children of FlexStatement should be lists
-                attrName, items = self.parse_list(child)
-                stmt[attrName] = items
+        children = dict([cls.parse_stmt_child(child) for child in elem])
+        stmt.update(children)
         return stmt
 
+    @classmethod
+    def parse_stmt_child(cls, elem):
+        parser = cls.stmt_child_parser(elem)
+        return parser(elem)
+
+    @classmethod
+    def stmt_child_parser(cls, elem):
+        # If the tag isn't one of the special cases defined in ``parsers``,
+        # by default treat it as a list container
+        parsers = {'AccountInformation': cls.parse_acctinfo,
+                   'ConversionRates': cls.parse_rates,
+                   'FxPositions': cls.parse_fxpos, }
+        return parsers.get(elem.tag, cls.parse_list)
+
     @staticmethod
-    def parse_rate(schema, elem, baseCurrency):
-        rate = schema.convert(elem)
+    def parse_acctinfo(elem):
+        return 'AccountInformation', schemata.AccountInformation.convert(elem)
+
+    @classmethod
+    def parse_rates(cls, elem):
+        return 'ConversionRates', dict(cls.parse_rate(rate) for rate in elem)
+
+    @staticmethod
+    def parse_rate(elem):
+        """
+        Use input schema to convert element.
+
+        Return duple of ((currency, date), rate) suitable for use
+        as a dict item.
+        """
+        rate = schemata.ConversionRate.convert(elem)
         return (rate['fromCurrency'], rate['reportDate']), rate['rate']
 
-    def parse_list(self, elem):
+    @classmethod
+    def parse_fxpos(cls, elem):
+        if len(elem) == 0:
+            items = []
+        else:
+            # Sanity check
+            if len(elem) != 1:
+                msg = "<{}> has more than 1 child: {}".format(
+                    elem.tag,
+                    ', '.join(["<{}>".format(child.tag) for child in elem])
+                )
+                raise FlexParserError(msg)
+            fxLots = elem[0]
+            if fxLots.tag != 'FxLots':
+                msg = "<{}> can only contain <FxLots>, not <{}>"
+                raise FlexParserError(msg.format(elem.tag, fxLots))
+
+            attrName, items = cls.parse_list(fxLots)
+
+        return 'FxPositions', items
+
+    @staticmethod
+    def parse_list(elem):
+        """
+        Use container tag to look up conversion schema for contained items.
+
+        Return duple of (container tag, list of converted items).
+        """
         tag = elem.tag
         try:
             schema = elementSchemata[tag]
         except KeyError:
-            raise  # FIXME
+            msg = "Don't know the schema to parse items in {}"
+            raise FlexParserError(msg.format(tag))
         items = [schema.convert(item) for item in elem]
         return tag, items
 
+    # subparsers = {'AccountInformation': parse_acctinfo,
+                  # 'ConversionRates': parse_rates,
+                  # 'FxPositions': parse_fxpos, }
 
 ##############################################################################
 # CLI SCRIPT
