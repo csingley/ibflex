b03257296505e12c1e47ba064a1aa9e2dc06b163
Christopher Singley <csingley@gmail.com>
2019-07-02 14:34:47 -0500

    client - rework with dataclasses instead of schemata

--- ibflex/client.py
+++ ibflex/client.py
@@ -5,15 +5,15 @@ Download Flex queries without logging into Account Management web page.
 https://www.interactivebrokers.com/en/software/am/am/reports/flex_web_service_version_3.htm
 """
 # stdlib imports
+from dataclasses import dataclass
 import xml.etree.ElementTree as ET
 from datetime import datetime
 import time
-import sys
+from typing import Union, Optional
 
 
 # 3rd party imports
 import requests
-from ibflex import fields, schemata
 
 
 ###############################################################################
@@ -28,169 +28,200 @@ STMT_URL = FLEX_URL + 'FlexStatementService.GetStatement'
 # ERRORS
 ###############################################################################
 ERRORS = [
-    ('1003', 'Statement is not available.'),
-    ('1004', 'Statement is incomplete at this time. Please try again shortly.'),
-    ('1005', 'Settlement data is not ready at this time. Please try again shortly.'),
-    ('1006', 'IFO P/L data is not ready at this time. Please try again shortly.'),
-    ('1007', 'MTM P/L data is not ready at this time. Please try again shortly.'),
-    ('1008', 'MTM and FIFO P/L data is not ready at this time. Please try again shortly.'),
-    ('1009', 'The server is under heavy load. Statement could not be generated at this time. Please try again shortly.'),
-    ('1010', 'Legacy Flex Queries are no longer supported. Please convert over to Activity Flex.'),
-    ('1011', 'Service account is inactive.'),
-    ('1012', 'Token has expired.'),
-    ('1013', 'IP restriction.'),
-    ('1014', 'Query is invalid.'),
-    ('1015', 'Token is invalid.'),
-    ('1016', 'Account in invalid.'),
-    ('1017', 'Reference code is invalid.'),
-    ('1018', 'Too many requests have been made from this token. Please try again shortly.'),
-    ('1019', 'Statement generation in progress. Please try again shortly.'),
-    ('1020', 'Invalid request or unable to validate request.'),
-    ('1021', 'Statement could not be retrieved at this time. Please try again shortly.'),
+    ("1003", "Statement is not available."),
+    ("1004", "Statement is incomplete at this time. Please try again shortly."),
+    ("1005", "Settlement data is not ready at this time. Please try again shortly."),
+    ("1006", "FIFO P/L data is not ready at this time. Please try again shortly."),
+    ("1007", "MTM P/L data is not ready at this time. Please try again shortly."),
+    ("1008", "MTM and FIFO P/L data is not ready at this time. Please try again shortly."),
+    ("1009", "The server is under heavy load. Statement could not be generated at this time. Please try again shortly."),
+    ("1010", "Legacy Flex Queries are no longer supported. Please convert over to Activity Flex."),
+    ("1011", "Service account is inactive."),
+    ("1012", "Token has expired."),
+    ("1013", "IP restriction."),
+    ("1014", "Query is invalid."),
+    ("1015", "Token is invalid."),
+    ("1016", "Account in invalid."),
+    ("1017", "Reference code is invalid."),
+    ("1018", "Too many requests have been made from this token. Please try again shortly."),
+    ("1019", "Statement generation in progress. Please try again shortly."),
+    ("1020", "Invalid request or unable to validate request."),
+    ("1021", "Statement could not be retrieved at this time. Please try again shortly."),
 ]
 ERROR_CODES, ERROR_MSGS = zip(*ERRORS)
+
+SERVER_BUSY = ("1009", "1019", )
+CLIENT_THROTTLED = ("1018", )
 
 
 class IbflexClientError(Exception):
     """ Base class for Exceptions defined in this module """
-    pass
 
 
 class BadResponseError(IbflexClientError):
     """
     Exception raised for malformed Flex response.
     """
-    def __init__(self, status, response):
-        self.status = status
+    def __init__(self, response: requests.Response):
         self.response = response
-        display = "Bad response, status='{}':\n\n {}".format(status, response)
-        super(BadResponseError, self).__init__(display)
+        super(BadResponseError, self).__init__(response.content)
 
 
 class ResponseCodeError(IbflexClientError):
     """
     Exception raised when Flex server returns a response with an error code.
     """
-    def __init__(self, code, msg):
+    def __init__(self, code: str, msg: str):
         self.code = code
         self.msg = msg
-        display = "Code={}: {}".format(code, msg)
-        super(ResponseCodeError, self).__init__(display)
+        super(ResponseCodeError, self).__init__(f"Code={code}: {msg}")
 
 
 ###############################################################################
-# SCHEMATA
+#  FlexStatementResponse TYPES
 ###############################################################################
-class TextSchema(metaclass=schemata.SchemaMetaclass):
-    """
-    Base schema class where data is in text not attributes.
-    """
-    @classmethod
-    def convert(cls, elem):
-        output = {field.tag: cls.fields[field.tag].convert(field.text)
-                  for field in elem}
-        return output
+@dataclass(frozen=True)
+class StatementAccess:
+    timestamp: datetime
+    ReferenceCode: str
+    Url: str
 
 
-class ResponseSuccessSchema(TextSchema):
-    Status = fields.OneOf('Success', 'Fail', 'Warn', required=True)
-    ReferenceCode = fields.String(required=True)
-    Url = fields.String(required=True)
-
-
-class ResponseFailureSchema(TextSchema):
-    Status = fields.OneOf('Success', 'Fail', 'Warn', required=True)
-    ErrorCode = fields.OneOf(*ERROR_CODES, required=True)
-    ErrorMessage = fields.OneOf(*ERROR_MSGS, required=True)
-
-
-ResponseSchemata = {'Success': ResponseSuccessSchema,
-                    'Fail': ResponseFailureSchema}
+@dataclass(frozen=True)
+class StatementError:
+    timestamp: datetime
+    ErrorCode: str
+    ErrorMessage: str
 
 
 ###############################################################################
 # FUNCTIONS
 ###############################################################################
-def requests_get_timeout(url, token, query_id):
+def download(token: str, query_id: str) -> bytes:
+    """2-step FlexQueryReport download process.
+
+    Args:
+        token: Current access token from Reports > Settings > FlexWeb Service.
+        query_id: Flex Query ID from
+                  Reports > Flex Queries > Custom Flex Queries > Configure.
+    """
+    stmt_access = request_statement(token, query_id)
+    status = 0
+    while status is not True:
+        time.sleep(status)
+        response = submit_request(
+            url=stmt_access.Url or STMT_URL,
+            token=token,
+            query=stmt_access.ReferenceCode,
+        )
+        status = check_statement_response(response)
+    return response.content
+
+
+def request_statement(
+    token: str, query_id: str, url: Optional[str] = None
+) -> StatementAccess:
+    """First part of the 2-step download process.
+    """
+    url = url or REQUEST_URL
+    response = submit_request(url, token, query=query_id)
+    stmt_access = parse_stmt_response(response)
+    if isinstance(stmt_access, StatementError):
+        raise ResponseCodeError(
+            stmt_access.ErrorCode,
+            stmt_access.ErrorMessage,
+        )
+    return stmt_access
+
+
+def submit_request(url: str, token: str, query: str) -> requests.Response:
+    """Post a query to an API access point, along with an authentication token.
+
+    Retry with a progressive timeout window.
+    """
+    MAX_REQUESTS = 3
+    TIMEOUT_INCREMENT = 5
+
     response = None
     req_count = 1
-    while(not response):
+    while (not response):
         try:
-            response = requests.get(url,
-                                    params={'v': '3', 't': token, 'q': query_id},
-                                    headers={'user-agent': 'Java'},
-                                    timeout=5*req_count)
+            response = requests.get(
+                url,
+                params={"v": "3", "t": token, "q": query},
+                headers={"user-agent": "Java"},
+                timeout=req_count * TIMEOUT_INCREMENT,
+            )
         except requests.exceptions.Timeout:
-            if(req_count<3):
-                req_count += 1
-                print('Request Timeout, re-sending...')
+            if req_count >= MAX_REQUESTS:
+                raise
             else:
-                sys.exit('Request Timeout, exiting.')
+                print("Request Timeout, re-sending...")
+                req_count += 1
 
     return response
 
 
-def send_request(token, query_id, url=None):
-    url = url or REQUEST_URL
-    response = requests_get_timeout(url, token, query_id)
-    response = ET.fromstring(response.content)
-    assert response.tag == 'FlexStatementResponse'
-    timestamp = response.attrib['timestamp']
-    # Convert "EST"/"EDT" to UTC offset so datetime.strptime can understand
-    tz = {'EST': '-0500', 'EDT': '-0400'}[timestamp[-3:]]
-    timestamp = timestamp[:-3] + tz
+def parse_stmt_response(
+    response: requests.Response
+) -> Union[StatementAccess, StatementError]:
+    """Read 1st step response; parse into StatementAccess or StatementError.
+    """
+    try:
+        elem = ET.fromstring(response.content)
+        assert elem.tag == 'FlexStatementResponse'
 
-    timestamp = datetime.strptime(timestamp, '%d %B, %Y %I:%M %p %z')
+        timestamp = elem.attrib['timestamp']
+        # Convert "EST"/"EDT" to UTC offset so datetime.strptime can understand
+        tz = {'EST': '-0500', 'EDT': '-0400'}[timestamp[-3:]]
+        timestamp = timestamp[:-3] + tz
+        datetime_ = datetime.strptime(timestamp, '%d %B, %Y %I:%M %p %z')
 
-    status = response.find('Status')
-    if status is None:
-        raise BadResponseError(status=status, response=response)
-    schema = ResponseSchemata.get(status.text, None)
-    if schema is None:
-        raise BadResponseError(status=status, response=response)
-    output = schema.convert(response)
-    output['timestamp'] = timestamp
-    return output
+        data = {child.tag: child.text for child in elem}
+        status = data.pop("Status")
+        assert status in {"Success", "Fail", "Warn"}
+        Type = {
+            "Success": StatementAccess,
+            "Fail": StatementError,
+            "Warn": StatementError,
+        }[status]
+        return Type(timestamp=datetime_, **data)  # type: ignore
+    except Exception:
+        raise BadResponseError(response=response)
 
 
-def get_statement(token, reference_code, url=None):
-    url = url or STMT_URL
-    statement = requests_get_timeout(url, token, reference_code)
-    return statement.content
+def check_statement_response(response: requests.Response) -> Union[bool, int]:
+    """Validate response received from 2nd step of download.
 
+    Returns:
+        True if `response` contains a FlexQueryResponse.
+        Retry delay (seconds) if `response` is an error indicating that
+            'please try again shortly' means 'within several seconds'.
 
-def download(token, query_id):
-    response = send_request(token, query_id)
-    status = response['Status']
-    if status == 'Success':
-        reference_code = response['ReferenceCode']
-        url = response['Url']
-        ready = False
-        while not ready:
-            response = get_statement(token, reference_code, url)
-            resp_str = str(response)
-            try:
-                if 'FlexQueryResponse' in resp_str:
-                    ready = True
-                elif 'FlexStatementResponse' in resp_str:
-                    output = ResponseFailureSchema.convert(ET.fromstring(response))
-                    raise ResponseCodeError(code=output['ErrorCode'],
-                                            msg=output['ErrorMessage'])
-                else:
-                    raise BadResponseError(status=status, response=response)
-            except ResponseCodeError as err:
-                if err.code in ('1019',):
-                    time.sleep(5)
-                elif err.code in ('1018',):
-                    time.sleep(10)
-                else:
-                    raise
-        return response
-    elif status == 'Fail':
-        raise ResponseCodeError(code=response['ErrorCode'],
-                                msg=response['ErrorMessage'])
+    Raises:
+        ResponseCodeError if `response` is any other kind of error.
+        BadResponseError if we can't parse `response`.
+    """
+    #  FlexQueryResponses can be massive; avoid parsing them.
+    resp_str = str(response.content)
+    if 'FlexQueryResponse' in resp_str:
+        return True
+    elif 'FlexStatementResponse' in resp_str:
+        try:
+            error = parse_stmt_response(response)
+            assert isinstance(error, StatementError)
+        except Exception:
+            raise BadResponseError(response)
+        if error.ErrorCode in SERVER_BUSY:
+            #  Statement generation in progress. Please try again shortly.
+            return 5
+        elif error.ErrorCode in CLIENT_THROTTLED:
+            #  Too many requests have been made from this token. Please try again shortly.
+            return 10
+        else:
+            raise ResponseCodeError(error.ErrorCode, error.ErrorMessage)
     else:
-        raise BadResponseError(status=status, response=response)
+        raise BadResponseError(response)
 
 
 ##############################################################################
---
+++ tests/test_client.py
@@ -0,0 +1,154 @@
+# coding: utf-8
+""" Unit tests for ibflex.parser module """
+
+import unittest
+from unittest.mock import Mock, patch, call
+import datetime
+from io import BytesIO
+
+import requests
+from ibflex import client, parser, Types
+
+
+RESPONSE_SUCCESS = (
+    '<FlexStatementResponse timestamp="28 August, 2012 10:37 AM EDT">'
+    '<Status>Success</Status>'
+    '<ReferenceCode>1234567890</ReferenceCode>'
+    '<Url>https://gdcdyn.interactivebrokers.com/Universal/servlet/FlexStatementService.GetStatement</Url>'
+    '</FlexStatementResponse>'
+)
+
+
+RESPONSE_FAIL = (
+    '<FlexStatementResponse timestamp="28 August, 2012 10:37 AM EDT">'
+    '<Status>Fail</Status>'
+    '<ErrorCode>1012</ErrorCode>'
+    '<ErrorMessage>Token has expired.</ErrorMessage>'
+    '</FlexStatementResponse>'
+)
+
+
+FLEX_QUERY_RESPONSE = (
+    '<FlexQueryResponse queryName="Test" type="AZ">'
+    '<FlexStatements count="1">'
+    '<FlexStatement accountId="U666777" />'
+    '</FlexStatements>'
+    '</FlexQueryResponse>'
+)
+
+
+def mock_response(*args, **kwargs) -> object:
+    class MockResponse:
+        def __init__(self, content: str):
+            self._content = content
+
+        @property
+        def content(self) -> bytes:
+            return bytes(self._content, encoding="utf8")
+
+    params = kwargs["params"]
+    token = params["t"]
+    query = params["q"]
+
+    if token == "DEADBEEF" and query == "0987654321":
+        return MockResponse(RESPONSE_SUCCESS)
+    elif token == "DEADBEEF" and query == "1234567890":
+        return MockResponse(FLEX_QUERY_RESPONSE)
+
+    return MockResponse(RESPONSE_FAIL)
+
+
+@patch("requests.get", side_effect=requests.exceptions.Timeout)
+class SubmitRequestTestCase(unittest.TestCase):
+    def test_submit_request_retry(self, mock_requests_get):
+        with self.assertRaises(requests.exceptions.Timeout):
+            client.submit_request(
+                url=client.REQUEST_URL,
+                token="DEADBEEF",
+                query="0987654321",
+            )
+
+        self.assertEqual(
+            mock_requests_get.call_args_list,
+            [
+                call(
+                    client.REQUEST_URL,
+                    params={"v": "3", "t": "DEADBEEF", "q": "0987654321"},
+                    headers={"user-agent": "Java"},
+                    timeout=5,
+                ),
+                call(
+                    client.REQUEST_URL,
+                    params={"v": "3", "t": "DEADBEEF", "q": "0987654321"},
+                    headers={"user-agent": "Java"},
+                    timeout=10,
+                ),
+                call(
+                    client.REQUEST_URL,
+                    params={"v": "3", "t": "DEADBEEF", "q": "0987654321"},
+                    headers={"user-agent": "Java"},
+                    timeout=15,
+                ),
+            ],
+        )
+
+
+@patch("requests.get", side_effect=mock_response)
+class RequestStatementTestCase(unittest.TestCase):
+    def test_request_statement(self, mock_requests_get):
+        #  `url` arg defaults to client.REQUEST_URL
+        output = client.request_statement(
+            token="DEADBEEF",
+            query_id="0987654321",
+        )
+
+        mock_requests_get.assert_called_once_with(
+            client.REQUEST_URL,
+            params={"v": "3", "t": "DEADBEEF", "q": "0987654321"},
+            headers={"user-agent": "Java"},
+            timeout=5,
+        )
+
+        self.assertIsInstance(output, client.StatementAccess)
+        self.assertEqual(
+            output.timestamp,
+            datetime.datetime(2012, 8, 28, 10, 37, tzinfo=datetime.timezone(
+                datetime.timedelta(hours=-4)))
+        )
+        self.assertEqual(output.ReferenceCode, "1234567890")
+        self.assertEqual(output.Url, client.STMT_URL)
+
+
+@patch("requests.get", side_effect=mock_response)
+class DownloadTestCase(unittest.TestCase):
+    def test_request_statement(self, mock_requests_get: Mock):
+        output = client.download(
+            token="DEADBEEF",
+            query_id="0987654321",
+        )
+        self.assertIsInstance(output, bytes)
+
+        self.assertEqual(
+            mock_requests_get.call_args_list,
+            [
+                call(
+                    client.REQUEST_URL,
+                    params={"v": "3", "t": "DEADBEEF", "q": "0987654321"},
+                    headers={"user-agent": "Java"},
+                    timeout=5,
+                ),
+                call(
+                    client.STMT_URL,
+                    params={"v": "3", "t": "DEADBEEF", "q": "1234567890"},
+                    headers={"user-agent": "Java"},
+                    timeout=5,
+                ),
+            ],
+        )
+
+        response = parser.parse(BytesIO(output))
+        self.assertIsInstance(response, Types.FlexQueryResponse)
+
+
+if __name__ == '__main__':
+    unittest.main(verbosity=3)
