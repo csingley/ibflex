e163891118a5fe32bb07c648013829f87e5e3737
Christopher Singley <csingley@gmail.com>
2019-07-02 17:14:12 -0500

    setup.py - implement PEP 561 packaging of type hints

---
+++ ibflex/__version__.py
@@ -0,0 +1,16 @@
+"""
+The Doomsday Machine is not sufficiently controllable. Even though it maximizes
+the probability that deterrence will work (including minimizing the probability
+of accidents or miscalculations), it is totally unsatisfactory.  One must still
+examine the consequences of a failure. In this case a failure kills too many
+people and kills them too automatically.
+"""
+__title__ = "ibflex"
+__description__ = ("Parse Interactive Brokers Flex XML reports and convert "
+                   "to Python data types"),
+__url__ = "https://github.com/csingley/ibflex"
+__version__ = "0.13"
+__author__ = "Christopher Singley"
+__author_email__ = "csingley@gmail.com"
+__license__ = "MIT"
+__copyright__ = "Copyright 2019 Christopher Singley"
--- setup.py
+++ setup.py
@@ -1,64 +1,68 @@
+"""
+Release process:
+    1. Update ibflex.__version__.__version__
+    2. Change download_url below
+    3. Commit changes & push
+    4. `git tag` the release
+    5. Push tags
+    6. Change download_url back to master; commit & push
+"""
 import os.path
 from setuptools import setup, find_packages
 
-# Get the long description from the relevant file
 __here__ = os.path.dirname(os.path.realpath(__file__))
-with open(os.path.join(__here__, 'README.rst'), 'r') as f:
-    long_description = f.read()
 
-version = '0.12'
+ABOUT = {}
+with open(os.path.join(__here__, "ibflex", "__version__.py"), "r") as f:
+    exec(f.read(), ABOUT)
+
+with open(os.path.join(__here__, "README.rst"), "r") as f:
+    README = f.read()
+
+URL_BASE = "{}/tarball".format(ABOUT["__url__"])
 
 setup(
-    name='ibflex',
-    version=version,
-    #  download_url='https://github.com/csingley/ibflex/tarball/master',
-    download_url='https://github.com/csingley/ibflex/releases/tag/' + version,
-    description=('Parse Interactive Brokers Flex XML reports and convert '
-                 'to Python data types'),
-    long_description=long_description,
-
-    url='https://github.com/csingley/ibflex',
-
-    author='Christopher Singley',
-    author_email='csingley@gmail.com',
-
-    license='MIT',
-
-    classifiers=[
-        'Development Status :: 3 - Alpha',
-        'Intended Audience :: Developers',
-        'Intended Audience :: Financial and Insurance Industry',
-        'Topic :: Software Development :: Libraries :: Python Modules',
-        'Topic :: Utilities',
-        'Topic :: Office/Business',
-        'Topic :: Office/Business :: Financial',
-        'Topic :: Office/Business :: Financial :: Accounting',
-        'Topic :: Office/Business :: Financial :: Investment',
-        'License :: OSI Approved :: MIT License',
-        'Operating System :: OS Independent',
-        'Natural Language :: English',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-    ],
-
-    keywords=['Interactive Brokers', 'ibkr', 'flex', 'xml'],
-
+    name=ABOUT["__title__"],
+    version=ABOUT["__version__"],
+    description=ABOUT["__description__"],
+    long_description=README,
+    long_description_content_type="text/x-rst",
+    author=ABOUT["__author__"],
+    author_email=ABOUT["__author_email__"],
+    url=ABOUT["__url__"],
     packages=find_packages(),
-
     package_data={
-        'ibflex': ['README.rst', 'tests/*'],
+        "ibflex": ["README.rst", "tests/*", "py.typed"],
     },
-
+    python_requires=">=3.7",
+    license=ABOUT["__license__"],
+    # Note: change 'master' to the tag name when releasing a new verion
+    #  download_url="{}/master".format(URL_BASE),
+    download_url="{}/{}".format(URL_BASE, ABOUT["__version__"]),
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Financial and Insurance Industry",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: Utilities",
+        "Topic :: Office/Business",
+        "Topic :: Office/Business :: Financial",
+        "Topic :: Office/Business :: Financial :: Accounting",
+        "Topic :: Office/Business :: Financial :: Investment",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Natural Language :: English",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.7",
+    ],
+    keywords=["Interactive Brokers", "ibkr", "flex", "xml"],
     extras_require={
-        'web': ['requests'],
+        "web": ["requests"],
     },
-
     entry_points={
-        'console_scripts': [
-            'flexget=ibflex.client:main [web]',
+        "console_scripts": [
+            "flexget=ibflex.client:main [web]",
         ],
     },
 )
