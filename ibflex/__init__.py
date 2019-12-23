7c5d5e9bdbe2c430814284c8629c47afca741f2f
Christopher Singley <csingley@gmail.com>
2019-12-23 04:40:58 -0600

    __init__ import __version__ metadata

--- ibflex/__init__.py
+++ ibflex/__init__.py
@@ -6,3 +6,14 @@ from . import parser
 from .parser import parse
 from . import utils
 from . import client
+
+from .__version__ import (
+    __title__,
+    __description__,
+    __url__,
+    __version__,
+    __author__,
+    __author_email__,
+    __license__,
+    __copyright__,
+)
