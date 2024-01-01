0cca9e0ba6ef174d6f51643357e0cf8cac98aac7
Dr-Nuke <drnuke.git@gmail.com>
2024-01-01 22:12:48 +0100

    urls adjusted. InLieu Type added

--- ibflex/Types.py
+++ ibflex/Types.py
@@ -575,6 +575,7 @@ class CashReportCurrency(FlexElement):
     paymentInLieu: Optional[decimal.Decimal] = None
     paymentInLieuSec: Optional[decimal.Decimal] = None
     paymentInLieuCom: Optional[decimal.Decimal] = None
+    paymentInLieuIBUKL:  Optional[decimal.Decimal] = None
     transactionTax: Optional[decimal.Decimal] = None
     transactionTaxSec: Optional[decimal.Decimal] = None
     transactionTaxCom: Optional[decimal.Decimal] = None
